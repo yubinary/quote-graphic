a2116df724fda725ca2ccc136a9943709fe4f38a
yubinary <yubinheo356@gmail.com>
2021-01-14 10:01:20 +0900

    Added styles for palette

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { ChromePicker } from 'react-color';
+import { BiHighlight, BiMinus, BiPlus } from 'react-icons/bi';
 
+import transparent from "./transparent.jpg";
 import Quote from "./Quote.js";
 
 import '../styles/Palette.css';
@@ -17,17 +19,19 @@ export default function Palette({ fetchQuote, quote }) {
   const [nthImage, setNthImage] = useState(0);
 
   const imgArray = [
-    "57e5d447485baa14f6da8c7dda7936771637dde454596c48732e7bd19249c250b9_1280.jpg",
+    "55e1d3464f55b108f5d0846096293e7d1d3cdfe25b4c704f742f7ed19348c759_1280.jpg",
+    "57e1dc444d57af14f6da8c7dda7936771637dde454596c48732e7bd19249c35bb8_1280.jpg",
+    "55e1d6464255a914f6da8c7dda7936771637dde454596c48732e7bd19249c35cbd_1280.jpg",
+    "57e2dd414953a814f6da8c7dda7936771637dde454596c48732e7bd19249c350be_1280.jpg",
     "57e1d5434857a814f6da8c7dda7936771637dde454596c48732e7bd19249c25eb9_1280.jpg",
+    "57e5d447485baa14f6da8c7dda7936771637dde454596c48732e7bd19249c250b9_1280.jpg",
     "54e2d04b4c5aad14f6da8c7dda7936771637dde454596c48732e7bd19249c25ab9_1280.jpg",
     "5ee2dc434a51b108f5d0846096293e7d1d3cdfe25b4c704f742f7ed1934bcd5e_1280.jpg",
     "57e2dd414953a814f6da8c7dda7936771637dde454596c48732e7bd19249c350be_1280.jpg",
     "55e1d6464255a914f6da8c7dda7936771637dde454596c48732e7bd19249c35cbd_1280.jpg",
     "54e0d5464a54aa14f6da8c7dda7936771637dde454596c48732e7bd19249c35cbd_1280.jpg",
     "54e6d4444b53ae14f6da8c7dda7936771637dde454596c48732e7bd19249c35cbd_1280.jpg",
-    "57e1dc444d57af14f6da8c7dda7936771637dde454596c48732e7bd19249c35bb8_1280.jpg",
     "57e3d2404b55ad14f6da8c7dda7936771637dde454596c48732e7bd19249c358be_1280.jpg",
-    "55e1d3464f55b108f5d0846096293e7d1d3cdfe25b4c704f742f7ed19348c759_1280.jpg",
     "54e8d1454857ad14f6da8c7dda7936771637dde454596c48732e7bd19249c05ab9_1280.jpg",
     "52e7d5434f53a814f6da8c7dda7936771637dde454596c48732e7bd19249c158bf_1280.jpg",
     "54e0d5474e5aaf14f6da8c7dda7936771637dde454596c48732e7bd19249c158bf_1280.jpg"
@@ -90,38 +94,61 @@ export default function Palette({ fetchQuote, quote }) {
     }
   }
 
+  if (imageUrl === "") {
+    var backgroundImageButtonStyle = {
+      backgroundImage: "url(" + transparent + ")"
+    }
+  } else {
+    var backgroundImageButtonStyle = {
+      backgroundImage: "url(" + imageUrl + ")",
+      backgroundSize: "cover"
+    }
+  }
+
+
   return (
     <div>
       <div className="palette">
-        <select onChange={(event) => setFontStyle(event.target.value)}>
-          <option value="Poppins, sans-serif">Poppins</option>
-          <option value="Georgia, serif">Georgia</option>
-          <option value="Courier, monospace">Courier</option>
-          <option value="Comic Sans MS, Comic Sans, cursive">Comic Sans</option>
-          <option value="Times, Times New Roman, serif">Times</option>
-          <option value="Arial Narrow, sans-serif">Arial Narrow</option>
-        </select>
-        <div className="palette-font-size">
-          <button onClick={handleClickDecrement}>-</button>
-          <p>{fontSize}</p>
-          <button onClick={handleClickIncrement}>+</button>
+        <div className="palette-font-style">
+          <select onChange={(event) => setFontStyle(event.target.value)}>
+            <option value="Poppins, sans-serif">Poppins</option>
+            <option value="Georgia, serif">Georgia</option>
+            <option value="Courier, monospace">Courier</option>
+            <option value="Comic Sans MS, Comic Sans, cursive">Comic Sans</option>
+            <option value="Times, Times New Roman, serif">Times</option>
+            <option value="Arial Narrow, sans-serif">Arial Narrow</option>
+          </select>
+          <div className="select-arrow"></div>
         </div>
-        <div>
+        <div className="palette-font-color">
           <select onChange={(event) => setFontColor(event.target.value)}>
             <option value="black">Black</option>
             <option value="white">White</option>
           </select>
+          <div className="select-arrow"></div>
         </div>
-        <div>
-          <button onClick={() => setIsDisplay(!isDisplay)}>Change Color</button>
+        <div className="palette-font-size">
+          <button onClick={handleClickDecrement}><BiMinus /></button>
+          <p>{fontSize}px</p>
+          <button onClick={handleClickIncrement}><BiPlus /></button>
+        </div>
+        <div className="palette-background-color">
+          <button onClick={() => setIsDisplay(!isDisplay)} />
           {displayColorPicker()}
         </div>
-        <div>
-          <button onClick={() => setIsImage(!isImage)}>Background Image</button>
+        <div className="palette-background-image">
+          <button onClick={() => setIsImage(!isImage)}
+            style={backgroundImageButtonStyle} />
           {displayRandomImages()}
         </div>
-        <button onClick={() => setIsHighlight(!isHighlight)}>Highlight</button>
-        <button onClick={fetchQuote}>New Quote</button>
+        <button
+          className="palette-highlight"
+          onClick={() => setIsHighlight(!isHighlight)}>
+          <BiHighlight />
+        </button>
+        <button
+          className="palette-new-quote"
+          onClick={fetchQuote}>New Quote</button>
       </div>
       <Quote
         quote={quote}
